0a51061ca3c481de30c04ba770b36f54325a97dd
Lanceky <evanslance100@gmail.com>
2024-10-10 19:56:10 +0000

    added documentation and implemented while loop that continues as long as there are keys to process

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
+"""Module for the canUnlockAll function."""
 
-canUnlockAll = __import__('0-lockboxes').canUnlockAll
 
-boxes = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes))
+def canUnlockAll(boxes):
+    """
+    Determine if all boxes can be opened.
 
-boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes))
+    Args:
+    boxes (list of lists): A list of boxes, where each box contains keys to other boxes.
 
-boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes))
+    Returns:
+    bool: True if all boxes can be opened, False otherwise.
+    """
+    n = len(boxes)
+    unlocked = set([0])  
+    keys = set(boxes[0])
 
+    while keys:
+        new_key = keys.pop()
+        if new_key < n and new_key not in unlocked:
+            unlocked.add(new_key)
+            keys.update(boxes[new_key])
+
+    return len(unlocked) == n
