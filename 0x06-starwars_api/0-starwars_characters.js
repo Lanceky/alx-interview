b16c290453e872bcd9cd6f0f1edacd69ec2f53ca
Lanceky <evanslance100@gmail.com>
2024-11-19 21:52:38 -0800

    update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,16 +1,8 @@
 #!/usr/bin/node
 
 const request = require('request');
-const FILM_ID = process.argv[2];
-const BASE_URL = 'https://swapi-api.alx-tools.com/api';
 
-if (!FILM_ID) {
-  console.error('Please provide a film ID');
-  process.exit(1);
-}
-
-// Promisify request to handle async operations better
-function makeRequest(url) {
+function makeRequest (url) {
   return new Promise((resolve, reject) => {
     request(url, (error, response, body) => {
       if (error) {
@@ -22,35 +14,39 @@ function makeRequest(url) {
   });
 }
 
-// Get character name from URL
-async function getCharacterName(url) {
+async function getCharacterName (url) {
   try {
     const character = await makeRequest(url);
     return character.name;
   } catch (error) {
-    console.error(`Error fetching character: ${error}`);
     return null;
   }
 }
 
-// Main function to get and print characters
-async function printMovieCharacters(filmId) {
+async function printMovieCharacters (filmId) {
   try {
     // Get film data
-    const film = await makeRequest(`${BASE_URL}/films/${filmId}/`);
+    const filmUrl = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
+    const film = await makeRequest(filmUrl);
 
-    // Get all character names in order
-    const characterPromises = film.characters.map(url => getCharacterName(url));
-    const characterNames = await Promise.all(characterPromises);
-
-    // Print character names
-    characterNames.forEach(name => {
-      if (name) console.log(name);
-    });
+    // Process characters sequentially to maintain order
+    for (const characterUrl of film.characters) {
+      const name = await getCharacterName(characterUrl);
+      if (name) {
+        console.log(name);
+      }
+    }
   } catch (error) {
-    console.error(`Error: ${error}`);
+    console.error('Error:', error);
     process.exit(1);
   }
 }
 
-printMovieCharacters(FILM_ID);
+// Get movie ID from command line argument
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Please provide a movie ID');
+  process.exit(1);
+}
+
+printMovieCharacters(movieId);
